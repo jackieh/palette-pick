4c1f388470a920bcc0836ab43ed5540517962402
Jackie Harris <hackiejarris@gmail.com>
2021-01-06 20:12:19 -0600

    Modify mkstripes-from-image test script.
    Rename output files to be listed differently when sorted in
    alphabetical order.
    
    Use only 4 tree depth levels when getting colors using quantization.

--- test/shell-scripts/mkstripes-from-image.sh
+++ test/shell-scripts/mkstripes-from-image.sh
@@ -23,44 +23,44 @@ for i in {1..8}; do
     n=$((4 * $i))
 
     printf "Creating posterized image with $n colors per channel..."
-    posterize_file_name=$input_file_base_name-posterized-$n.png
-    posterize_file_path=$output_directory_path/$posterize_file_name
+    posterized_file_name=$input_file_base_name-posterized-$n.png
+    posterized_file_path=$output_directory_path/$posterized_file_name
     start_time=`date +%s`
-    convert $input_file_path -colorspace HSL -posterize $n $posterize_file_path
+    convert $input_file_path -colorspace HSL -posterize $n $posterized_file_path
     end_time=`date +%s`
     printf " done in `expr $end_time - $start_time` seconds\n"
 
     printf "Creating quantized image with $n colors..."
-    quantize_file_name=$input_file_base_name-quantized-$n.png
-    quantize_file_path=$output_directory_path/$quantize_file_name
+    quantized_file_name=$input_file_base_name-quantized-$n.png
+    quantized_file_path=$output_directory_path/$quantized_file_name
     start_time=`date +%s`
-    convert $input_file_path -colors $n $quantize_file_path
+    convert $input_file_path -colors $n $quantized_file_path
     end_time=`date +%s`
     printf " done in `expr $end_time - $start_time` seconds\n"
 
     printf "Creating stripes images for $n colors using kmeans..."
     start_time=`date +%s`
-    stripes_file_name=$n-colors-stripes-kmeans-from-original.png
+    stripes_file_name=$n-colors-stripes-original-kmeans.png
     stripes_file_path=$output_directory_path/$stripes_file_name
     ./build/getcolors -m kmeans -n $n -I $input_file_path \
         | ./build/mkstripes -O $stripes_file_path
-    stripes_file_name=$n-colors-stripes-kmeans-from-posterized.png
+    stripes_file_name=$n-colors-stripes-posterized-kmeans.png
     stripes_file_path=$output_directory_path/$stripes_file_name
-    ./build/getcolors -m kmeans -n $n -I $posterize_file_path \
+    ./build/getcolors -m kmeans -n $n -I $posterized_file_path \
         | ./build/mkstripes -O $stripes_file_path
     end_time=`date +%s`
     printf " done in `expr $end_time - $start_time` seconds\n"
 
-    for d in {1..8}; do
+    for d in {1..4}; do
         printf "Creating stripes images for $n colors using quantization with tree depth $d..."
         start_time=`date +%s`
-        stripes_file_name=$n-colors-stripes-quantized-from-original-tree-depth-$d.png
+        stripes_file_name=$n-colors-stripes-original-quantized-tree-depth-$d.png
         stripes_file_path=$output_directory_path/$stripes_file_name
         ./build/getcolors -m quantize -n $n -d $d -I $input_file_path \
             | ./build/mkstripes -O $stripes_file_path
-        stripes_file_name=$n-colors-stripes-quantized-from-posterized-tree-depth-$d.png
+        stripes_file_name=$n-colors-stripes-posterized-quantized-tree-depth-$d.png
         stripes_file_path=$output_directory_path/$stripes_file_name
-        ./build/getcolors -m quantize -n $n -d $d -I $posterize_file_path \
+        ./build/getcolors -m quantize -n $n -d $d -I $posterized_file_path \
             | ./build/mkstripes -O $stripes_file_path
         end_time=`date +%s`
         printf " done in `expr $end_time - $start_time` seconds\n"
