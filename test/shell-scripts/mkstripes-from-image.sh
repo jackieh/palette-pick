484b4d1dff9f0e55e946aae3de3370d5a056ba65
Colton Lewis <colton.w.lewis@protonmail.com>
2021-02-27 18:20:50 -0600

    Use time builtin to measure operations rather than using date before and after. This is more efficient by avoiding multiple executions of date and reuses a more flexible existing solution that gives better formatting and opens the possibility of measuring CPU time.

--- test/shell-scripts/mkstripes-from-image.sh
+++ test/shell-scripts/mkstripes-from-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 input_file_path="$1"
+TIMEFORMAT=" done in %lR"
 
 if [[ -z "$input_file_path" ]]
 then
@@ -25,82 +26,72 @@ for i in {1..8}; do
     printf "Creating posterized image with $n colors per channel..."
     posterized_file_name="$input_file_base_name"-posterized-$n.png
     posterized_file_path="$output_directory_path"/"$posterized_file_name"
-    start_time=`date +%s`
-    convert "$input_file_path" -colorspace HSL -posterize $n "$posterized_file_path"
-    end_time=`date +%s`
-    printf " done in `expr $end_time - $start_time` seconds\n"
+    time convert "$input_file_path" -colorspace HSL -posterize $n "$posterized_file_path"
 
     printf "Creating quantized image with $n colors..."
     quantized_file_name="$input_file_base_name"-quantized-$n.png
     quantized_file_path="$output_directory_path"/"$quantized_file_name"
-    start_time=`date +%s`
-    convert "$input_file_path" -colors $n "$quantized_file_path"
-    end_time=`date +%s`
-    printf " done in `expr $end_time - $start_time` seconds\n"
+    time convert "$input_file_path" -colors $n "$quantized_file_path"
 
     printf "Creating stripes images for $n colors using kmeans..."
-    start_time=`date +%s`
+    time {
+	stripes_file_name=$n-colors-stripes-original-kmeans-random-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-random-spread -n $n -I "$input_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-posterized-kmeans-random-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-random-spread -n $n -I "$posterized_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
 
-    stripes_file_name=$n-colors-stripes-original-kmeans-random-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-random-spread -n $n -I "$input_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-    stripes_file_name=$n-colors-stripes-posterized-kmeans-random-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-random-spread -n $n -I "$posterized_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-original-kmeans-static-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-static-spread -n $n -I "$input_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-posterized-kmeans-static-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-static-spread -n $n -I "$posterized_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
 
-    stripes_file_name=$n-colors-stripes-original-kmeans-static-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-static-spread -n $n -I "$input_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-    stripes_file_name=$n-colors-stripes-posterized-kmeans-static-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-static-spread -n $n -I "$posterized_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-original-kmeans-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-hue-spread -n $n -I "$input_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-posterized-kmeans-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-hue-spread -n $n -I "$posterized_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
 
-    stripes_file_name=$n-colors-stripes-original-kmeans-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-hue-spread -n $n -I "$input_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-    stripes_file_name=$n-colors-stripes-posterized-kmeans-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-hue-spread -n $n -I "$posterized_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-original-kmeans-bright-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-bright-hue-spread -n $n -I "$input_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-posterized-kmeans-bright-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-bright-hue-spread -n $n -I "$posterized_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
 
-    stripes_file_name=$n-colors-stripes-original-kmeans-bright-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-bright-hue-spread -n $n -I "$input_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-    stripes_file_name=$n-colors-stripes-posterized-kmeans-bright-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-bright-hue-spread -n $n -I "$posterized_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-
-    stripes_file_name=$n-colors-stripes-original-kmeans-saturated-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-saturated-hue-spread -n $n -I "$input_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-    stripes_file_name=$n-colors-stripes-posterized-kmeans-saturated-hue-spread.png
-    stripes_file_path="$output_directory_path"/"$stripes_file_name"
-    ./build/getcolors -m kmeans-saturated-hue-spread -n $n -I "$posterized_file_path" \
-        | ./build/mkstripes -O "$stripes_file_path"
-
-    end_time=`date +%s`
-    printf " done in `expr $end_time - $start_time` seconds\n"
+	stripes_file_name=$n-colors-stripes-original-kmeans-saturated-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-saturated-hue-spread -n $n -I "$input_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+	stripes_file_name=$n-colors-stripes-posterized-kmeans-saturated-hue-spread.png
+	stripes_file_path="$output_directory_path"/"$stripes_file_name"
+	./build/getcolors -m kmeans-saturated-hue-spread -n $n -I "$posterized_file_path" \
+            | ./build/mkstripes -O "$stripes_file_path"
+    }
 
     for d in {1..4}; do
         printf "Creating stripes images for $n colors using quantization with tree depth $d..."
-        start_time=`date +%s`
-        stripes_file_name=$n-colors-stripes-original-quantized-tree-depth-$d.png
-        stripes_file_path="$output_directory_path"/"$stripes_file_name"
-        ./build/getcolors -m quantize -n $n -d $d -I "$input_file_path" \
-            | ./build/mkstripes -O "$stripes_file_path"
-        stripes_file_name=$n-colors-stripes-posterized-quantized-tree-depth-$d.png
-        stripes_file_path="$output_directory_path"/"$stripes_file_name"
-        ./build/getcolors -m quantize -n $n -d $d -I "$posterized_file_path" \
-            | ./build/mkstripes -O "$stripes_file_path"
-        end_time=`date +%s`
-        printf " done in `expr $end_time - $start_time` seconds\n"
+	time {
+            stripes_file_name=$n-colors-stripes-original-quantized-tree-depth-$d.png
+            stripes_file_path="$output_directory_path"/"$stripes_file_name"
+            ./build/getcolors -m quantize -n $n -d $d -I "$input_file_path" \
+		| ./build/mkstripes -O "$stripes_file_path"
+            stripes_file_name=$n-colors-stripes-posterized-quantized-tree-depth-$d.png
+            stripes_file_path="$output_directory_path"/"$stripes_file_name"
+            ./build/getcolors -m quantize -n $n -d $d -I "$posterized_file_path" \
+		| ./build/mkstripes -O "$stripes_file_path"
+	}
     done;
 done
